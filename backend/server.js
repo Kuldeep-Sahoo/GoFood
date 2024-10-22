52bd0817c3466bfec870eb9651ee0277638c13d0
Kuldeep Sahoo <144123998+Kuldeep-Sahoo@users.noreply.github.com>
2024-10-22 17:53:13 +0530

    Fix frontend issues

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,28 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoDB = require("./db");
 
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://kuldeep-gofood.onrender.com/"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://kuldeep-gofood.onrender.com/"); // Allow requests from all origins
   res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000" || process.env.ALLOWED_ORIGIN
-  );
-  res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
+
 // mongoDB();
 app.get("/", (req, res) => {
   res.send("Kuldeep");
